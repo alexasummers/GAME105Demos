59b575cc0c696f881ebcdc47f898a89fef5e4455
Alexa Summers <alexas0202@gmail.com>
2022-09-21 15:12:23 -0500

    Add collider and trigger demo

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Vector3 movement;
+    private Vector3 _movement;
     private CharacterController controller;
     private float moveX = 0;
     private float moveZ = 0;
-    public float playerMovementSpeed = 15;
+    public float playerMovementSpeed = 30;
     private Vector3 rotationX1;
     private Vector3 rotationX2;
     private Vector3 rotationZ1;
     private Vector3 rotationZ2;
 
+
+  
     void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
@@ -22,30 +24,35 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveX = Input.GetAxis("Horizontal")/playerMovementSpeed;
-        moveZ = Input.GetAxis("Vertical")/playerMovementSpeed;
-        movement = new Vector3(moveX, 0, moveZ);
+        moveX = Input.GetAxis("Horizontal") / playerMovementSpeed;
+        moveZ = Input.GetAxis("Vertical") / playerMovementSpeed;
+        _movement = new Vector3(moveX, 0, moveZ);
+        controller.Move(_movement);
 
-        rotationX1 = new Vector3 (-90, 0, 0);
-        rotationX2 = new Vector3 (90, 0, 0);
-        rotationZ1 = new Vector3 (0, 0, -90);
-        rotationZ2 = new Vector3 (0, 0, 90);
-        controller.Move(movement);
+        rotationX1 = new Vector3(-90, 0, 0);
+        rotationX2 = new Vector3(90, 0, 0);
+        rotationZ1 = new Vector3(0, 0, -90);
+        rotationZ2 = new Vector3(0, 0, 90);
 
-        if(moveX < 0) {
+        if(moveX < 0)
+        {
             transform.LookAt(rotationX1);
         }
 
-        if(moveX > 0) {
+        if(moveX > 0)
+        {
             transform.LookAt(rotationX2);
         }
 
-        if(moveZ < 0) {
+        if(moveZ < 0)
+        {
             transform.LookAt(rotationZ1);
         }
 
-        if(moveZ > 0){
+        if(moveZ > 0)
+        {
             transform.LookAt(rotationZ2);
         }
+
     }
 }
